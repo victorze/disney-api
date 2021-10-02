e7cb28edcb9ffcd66a54c0fa7b770cb20a11dbe7
victorze <victorze@protonmail.com>
2021-10-01 20:47:33 -0500

    Send welcome email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,27 @@
+const sgMail = require('@sendgrid/mail')
 const db = require('../models')
 const { UserExistsError, IncorrectCredentials } = require('./errors/authError')
 const { catchErrors } = require('../middleware/errors')
 
 const User = db.User
+
+const sendWelcomeEmail = async (to) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const msg = {
+    to: to,
+    from: process.env.MAIL_FROM_ADDRESS,
+    subject: 'Welcome to Disney API!',
+    text: 'Enjoy!',
+    html: '<h3 style="text-align: center;">Enjoy!<h3>',
+  }
+
+  try {
+    await sgMail.send(msg)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 const register = async (req, res) => {
   const [userExists] = await User.findAll({ where: { email: req.body.email } })
@@ -14,6 +33,11 @@ const register = async (req, res) => {
     user.setPassword(req.body.password)
     await user.save()
     const token = user.generateJwt()
+
+    if (process.env.NODE_ENV !== 'test') {
+      await sendWelcomeEmail(user.email)
+    }
+
     res.status(201).json({ token })
   }
 }
