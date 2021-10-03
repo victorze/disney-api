e904e95faccd6df7a26aed82f5e79585d1065c5a
victorze <victorze@protonmail.com>
2021-10-03 12:07:03 -0500

    Add Heroku server

--- docs/servers.js
+++ docs/servers.js
@@ -1,6 +1,10 @@
 module.exports = {
   servers: [
     {
+      url: 'https://victorze-disney-api.herokuapp.com/',
+      description: 'Heroku server',
+    },
+    {
       url: 'http://localhost:3500/',
       description: 'Local server',
     },
